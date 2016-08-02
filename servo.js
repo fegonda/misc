347ec9fc01bdb9b62a96c8da2d01d79c60fb18f6
Felix Gonda <felix.gonda@analog.com>
2016-08-02 10:33:02 -0400

    updating servo

--- servo.js
+++ servo.js
@@ -1,62 +1,59 @@
 var wpi = require('wiring-pi');
 
-class Servo
+function Servo()
 {
-    constructor(settings)
+    this.settings = settings;
+    this.angle = 0;
+    this.target = 0;
+    this.direction = 1;
+    this.callback = null;
+}
+
+Servo.prototype.move = function(target, callback) 
+{
+    this.target = target;
+    this.callback = callback;
+
+    if (this.target == this.angle) {
+        if (this.callback != null)
+        {
+            this.callback();
+        }
+    }
+    else if (this.target < this.angle) 
     {
-        this.settings = settings;
-        this.angle = 0;
-        this.target = 0;
+        this.direction = -1;
+    }
+    else 
+    {
         this.direction = 1;
-        this.callback = null;
+    }
+};
+
+Servo.prototype.update = function() 
+{
+    var prevAngle = this.angle;
+
+    if (this.direction < 0) 
+    {
+        this.angle = Math.max(this.angle - this.settings.incAngle, this.target);
+    }
+    else 
+    {
+        this.angle = Math.min(this.angle + this.settings.incAngle, this.target);
     }
 
-    move(target, callback) 
+    /* write data to the signal pin of the servo */
+    wpi.pwmWrite(this.settings.pin, this.angle);
+
+    /* first time we reache target, fire the callback */
+    if (this.angle != prevAngle && this.angle == this.target) 
     {
-        this.target = target;
-        this.callback = callback;
-
-        if (this.target == this.angle) {
-            if (this.callback != null)
-            {
-                this.callback();
-            }
-        }
-        else if (this.target < this.angle) 
+        if (this.callback != null) 
         {
-            this.direction = -1;
+            this.callback();
         }
-        else 
-        {
-            this.direction = 1;
-        }
-    };
-
-    update() 
-    {
-        var prevAngle = this.angle;
-
-        if (this.direction < 0) 
-        {
-            this.angle = Math.max(this.angle - this.settings.incAngle, this.target);
-        }
-        else 
-        {
-            this.angle = Math.min(this.angle + this.settings.incAngle, this.target);
-        }
-
-        /* write data to the signal pin of the servo */
-        wpi.pwmWrite(this.settings.pin, this.angle);
-
-        /* first time we reache target, fire the callback */
-        if (this.angle != prevAngle && this.angle == this.target) 
-        {
-            if (this.callback != null) 
-            {
-                this.callback();
-            }
-        }           
-    }
+    }           
 };
 
 /* export the class */
